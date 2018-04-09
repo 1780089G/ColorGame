4ac49be73fcd39284092d913f9695aba5b88f430
Unknown <1780089G@student.tp.edu.sg>
2018-04-09 15:06:15 +0800

    942108

--- ColorGame.js
+++ ColorGame.js
@@ -5,6 +5,7 @@ var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 colorDisplay.textContent = pickedColor;
+var h1 = document.querySelector("h1");
 
 for(var i = 0; i < squares.length; i++){
   // add initial colors to square
@@ -17,6 +18,7 @@ for(var i = 0; i < squares.length; i++){
    if(clickedColor === pickedColor){
       messageDisplay.textContent = ("Correct!");
       changeColors(clickedColor);
+      h1.style.background = clickedColor;
    } else {
      this.style.background = "#232323";
      messageDisplay.textContent = ("try display")
